ac823b391f982eafb19edcc9c19338b1f26e075d
codingpalette <codingpalette@gmail.com>
2020-09-09 11:40:55 +0900

    updateProduct 작업

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -18,10 +18,33 @@ export class ProductsService {
     }
 
     getSingleProduct(productId: string) {
-        const product = this.products.find((prod) => prod.id === productId);
+        const product = this.findProduct(productId)[0];
+        return {...product};
+    }
+
+    updateProduct(productId: string, title: string, desc: string, price: number) {
+        // const product = this.findProduct(productId)[0];
+        // const index = this.findProduct(productId)[1];
+        const [product, index] = this.findProduct(productId);
+        const updateProduct = {...product}
+        if (title) {
+            updateProduct.title = title;
+        }
+        if (desc) {
+            updateProduct.description = desc;
+        }
+        if (price) {
+            updateProduct.price = price;
+        }
+        this.products[index] = updateProduct;
+    }
+
+    private findProduct(id: string): [Product, number] {
+        const productIndex = this.products.findIndex((prod) => prod.id === id);
+        const product = this.products[productIndex];
         if (!product) {
             throw new NotFoundException('존재하지 않는 상품입니다.')
         }
-        return {...product};
+        return [product, productIndex];
     }
 }
